fd52cfa7833be51681bf61b6a1a0a36b0871966f
code-sanchu <a.ruben00001@gmail.com>
2024-09-05 15:29:18 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<h1 class="text-red-600">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="test">
+	<p>HELLO</p>
+</div>
+
+<style lang="sass">
+	.test 
+		p
+			@apply bg-blue-400 text-red-800
+</style>
